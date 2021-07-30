63a02822cc83f22a6c622d8033ee73f04e5c28e0
Jaehan <jbae12@my.centennialcollege.ca>
2021-07-29 21:45:51 -0400

    Testing (xUnit)

---
+++ Catalog.Api/Extensions.cs
@@ -0,0 +1,14 @@
+using System.Security.Policy;
+using Catalog.Api.Dtos;
+using Catalog.Api.Entities;
+
+namespace Catalog.Api
+{
+	public static class Extensions
+	{
+		public static ItemDto AsDto(this Item item)
+		{
+			return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
+		}
+	}
+}
